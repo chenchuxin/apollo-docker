e7b5e4cca4b2ecd0a47a854f4744abcc376c146c
ccx <idesireccx@163.com>
2018-03-02 18:18:54 +0800

    refactor: 去掉基础镜像的删除操作，最终镜像用jre不用jdk

--- Dockerfile
+++ Dockerfile
@@ -7,19 +7,17 @@ COPY src/settings.xml /usr/share/maven/conf/
 COPY build.sh /scripts/ 
 WORKDIR /src
 RUN tar zxf ${package_name} --strip-components=1 \
-    && rm ${package_name} \
     && cp /scripts/build.sh scripts/ \
-    && chmod 777 scripts/*.sh
+    && chmod 777 scripts/build.sh
 RUN scripts/build.sh
 RUN mkdir /app \
     && cp apollo-configservice/target/apollo-configservice-${version}.jar /app/configservice.jar \
     && cp apollo-adminservice/target/apollo-adminservice-${version}.jar /app/adminservice.jar \
     && cp apollo-portal/target/apollo-portal-${version}.jar /app/portal.jar
 RUN apk add -U tzdata \
-    && cp /usr/share/zoneinfo/Asia/Shanghai /etc/localtime \
-    && apk del tzdata
+    && cp /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 
-FROM java:8-jdk-alpine AS base
+FROM java:8-jre-alpine
 WORKDIR /app
 COPY --from=build /app .
 COPY --from=build /etc/localtime /etc/localtime
--- build.sh
+++ build.sh
@@ -1,30 +1,7 @@
 #!/bin/sh
 
-# apollo config db info
-apollo_config_db_url=jdbc:mysql://localhost:3306/ApolloConfigDB?characterEncoding=utf8
-apollo_config_db_username=root
-apollo_config_db_password=
-
-# apollo portal db info
-apollo_portal_db_url=jdbc:mysql://localhost:3306/ApolloPortalDB?characterEncoding=utf8
-apollo_portal_db_username=root
-apollo_portal_db_password=
-
-# meta server url, different environments should have different meta server addresses
-dev_meta=http://localhost:8080
-
-META_SERVERS_OPTS="-Ddev_meta=$dev_meta"
-
-# package config-service and admin-service
 echo "==== starting to build config-service and admin-service ===="
-
-mvn clean package -DskipTests -pl apollo-configservice,apollo-adminservice -am -Dapollo_profile=github -Dspring_datasource_url=$apollo_config_db_url -Dspring_datasource_username=$apollo_config_db_username -Dspring_datasource_password=$apollo_config_db_password
-
-echo "==== building config-service and admin-service finished ===="
+mvn clean package -DskipTests -pl apollo-configservice,apollo-adminservice -am -Dapollo_profile=github -Dspring_datasource_url=jdbc:mysql://localhost:3306/ApolloConfigDB?characterEncoding=utf8 -Dspring_datasource_username=root
 
 echo "==== starting to build portal ===="
-
-mvn clean package -DskipTests -pl apollo-portal -am -Dapollo_profile=github,auth -Dspring_datasource_url=$apollo_portal_db_url -Dspring_datasource_username=$apollo_portal_db_username -Dspring_datasource_password=$apollo_portal_db_password $META_SERVERS_OPTS
-
-echo "==== building portal finished ===="
-
+mvn clean package -DskipTests -pl apollo-portal -am -Dapollo_profile=github,auth -Dspring_datasource_url=jdbc:mysql://localhost:3306/ApolloPortalDB?characterEncoding=utf8 -Dspring_datasource_username=root -Ddev_meta=http://localhost:8080
